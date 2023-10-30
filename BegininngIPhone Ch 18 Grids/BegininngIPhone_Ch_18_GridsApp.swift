41d1c458f9aba9a96faa760f794f4bd49eccac2e
neddington <neddington@cascadia.edu>
2023-10-30 16:31:51 -0700

    Initial Commit

---
+++ BegininngIPhone Ch 18 Grids/BegininngIPhone_Ch_18_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BegininngIPhone_Ch_18_GridsApp.swift
+//  BegininngIPhone Ch 18 Grids
+//
+//  Created by Eddington, Nick on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct BegininngIPhone_Ch_18_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BegininngIPhone Ch 18 Grids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BegininngIPhone Ch 18 Grids
+//
+//  Created by Eddington, Nick on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
